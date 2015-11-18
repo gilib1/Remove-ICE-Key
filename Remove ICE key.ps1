366a6c269a68c869d9b8114fbdd60d829588dfba
gilib1 <gilbertpieris@gmail.com>
2015-11-18 11:15:03 +0000

    Remove ICE Key
    This is Powershell script to detect if ICE registry key does exist and
    it will remove it.

---
+++ Remove ICE key.ps1
@@ -0,0 +1,27 @@
+﻿Function Check-Ice
+{
+    Try
+        {
+            #Checks if ICE regkey exists
+            $ice = get-item -path HKLM:\HKEY_LOCAL_MACHINE\SOFTWARE\Rackspace\ICE -ErrorAction SilentlyContinue
+
+            if ($ice)
+                { 
+                    #Remove ICE regkey if exists 
+                    remove-item -path HKLM:\HKEY_LOCAL_MACHINE\SOFTWARE\Rackspace\ICE -ErrorAction SilentlyContinue $IceCheck = "Key deleted" 
+                }
+
+            else
+                {
+                    #If ICE registry key did not exist or present  
+                    $IceCheck = "Key not present" 
+                }
+
+            return $IceCheck
+         }
+    Catch
+        { 
+            Return "Unexpected Error" 
+        }
+
+}
